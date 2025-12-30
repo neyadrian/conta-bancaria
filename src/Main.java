c5e7434b0a8a095c07f3daaf5b284d624cfa0180
neyadrian <neyadrian2018@gmail.com>
2025-12-29 21:05:30 -0300

    update

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,55 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.util.Scanner;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite o nome do titular: ");
+        String nome = scanner.nextLine();
+
+        Conta conta = new Conta(nome);
+
+        int opcao;
+
+        do {
+            System.out.println("\n--- MENU ---");
+            System.out.println("1 - Depositar");
+            System.out.println("2 - Sacar");
+            System.out.println("3 - Consultar saldo");
+            System.out.println("0 - Sair");
+            System.out.print("Escolha uma opção: ");
+
+            opcao = scanner.nextInt();
+
+            switch (opcao) {
+                case 1:
+                    System.out.print("Valor para depósito: ");
+                    double deposito = scanner.nextDouble();
+                    conta.depositar(deposito);
+                    break;
+
+                case 2:
+                    System.out.print("Valor para saque: ");
+                    double saque = scanner.nextDouble();
+                    conta.sacar(saque);
+                    break;
+
+                case 3:
+                    System.out.println("Saldo atual: R$ " + conta.getSaldo());
+                    break;
+
+                case 0:
+                    System.out.println("Encerrando o sistema...");
+                    break;
+
+                default:
+                    System.out.println("Opção inválida.");
+            }
+
+        } while (opcao != 0);
+
+        scanner.close();
     }
 }
